80d3d651431e991a94858985e5c9df8b1f6ad38f
EXT02D49183 <gunaydin.tolga@consultant.turkcell.com.tr>
2022-06-24 22:37:45 +0300

    Css regulations were  done

--- src/components/BreadCrumb/BreadCrumb.tsx
+++ src/components/BreadCrumb/BreadCrumb.tsx
@@ -6,13 +6,13 @@ const BreadCrumb = () => {
     <div className='breadcrumb'>
       <ul className='breadcrumb__list'>
         <li className='list-item'>
-          <a href='!#' className='list-link'> Çiçek Sepeti <i className="arrow right"></i></a>
+          <a href='#!' className='list-link'> Çiçek Sepeti <i className="arrow right"></i></a>
         </li>
         <li className='list-item'>
-          <a href='!#' className='list-link'> İstanbul <i className="arrow right"></i></a>
+          <a href='#!' className='list-link'> İstanbul <i className="arrow right"></i></a>
         </li>
         <li className='list-item'>
-          <a href='!#' className='list-link'> CiceksepetiBreadCrumb </a>
+          <span> CiceksepetiBreadCrumb </span>
         </li>
       </ul>
     </div>
