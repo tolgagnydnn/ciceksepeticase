4131cfa42a70fd13c6cd3f7ec5fccc6e07481dfc
EXT02D49183 <gunaydin.tolga@consultant.turkcell.com.tr>
2022-06-24 23:35:12 +0300

    Footer menu was created

--- src/views/homepage/Footer/components/FooterMenu/FooterMenu.tsx
+++ src/views/homepage/Footer/components/FooterMenu/FooterMenu.tsx
@@ -1,131 +1,32 @@
 // Styles
 import './style.scss'
 
+// Data
+import footerMenuData from './footerMenuData'
+
 const FooterMenu = () => {
   return (
     <div className='footermenu'>
-      <div className='footermenu__menubox'>
-        <div className='menubox-title'>
-          <p> Faydalı Bilgiler</p>
+      {footerMenuData.map((menuitem: any) => (
+        <div className='footermenu__menubox'>
+          <div className='menubox-title'>
+            <p> {menuitem.title} </p>
+          </div>
+          <div className='menubox-menu'>
+            <ul>
+              {menuitem.menu.map((item: any) => (
+                <li>
+                  <a href='!#'> {item.title} </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className='menubox-menu'>
-          <ul>
-            <li>
-              <a href='!#'> Çiçek Bakımı </a>
-            </li>
-            <li>
-              <a href='!#'> Çiçek Eşliğinde Notlar </a>
-            </li>
-            <li>
-              <a href='!#'> Çiçek Anlamları </a>
-            </li>
-            <li>
-              <a href='!#'> Özel Günler </a>
-            </li>
-            <li>
-              <a href='!#'> Mevsimlere Göre Çiçekler </a>
-            </li>
-            <li>
-              <a href='!#'> BonnyFood Saklama Koşulları </a>
-            </li>
-            <li>
-              <a href='!#'> site Haritası </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className='footermenu__menubox'>
-        <div className='menubox-title'>
-          <p> Faydalı Bilgiler</p>
-        </div>
-        <div className='menubox-menu'>
-          <ul>
-            <li>
-              <a href='!#'> Çiçek Bakımı </a>
-            </li>
-            <li>
-              <a href='!#'> Çiçek Eşliğinde Notlar </a>
-            </li>
-            <li>
-              <a href='!#'> Çiçek Anlamları </a>
-            </li>
-            <li>
-              <a href='!#'> Özel Günler </a>
-            </li>
-            <li>
-              <a href='!#'> Mevsimlere Göre Çiçekler </a>
-            </li>
-            <li>
-              <a href='!#'> BonnyFood Saklama Koşulları </a>
-            </li>
-            <li>
-              <a href='!#'> site Haritası </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className='footermenu__menubox'>
-        <div className='menubox-title'>
-          <p> Faydalı Bilgiler</p>
-        </div>
-        <div className='menubox-menu'>
-          <ul>
-            <li>
-              <a href='!#'> Çiçek Bakımı </a>
-            </li>
-            <li>
-              <a href='!#'> Çiçek Eşliğinde Notlar </a>
-            </li>
-            <li>
-              <a href='!#'> Çiçek Anlamları </a>
-            </li>
-            <li>
-              <a href='!#'> Özel Günler </a>
-            </li>
-            <li>
-              <a href='!#'> Mevsimlere Göre Çiçekler </a>
-            </li>
-            <li>
-              <a href='!#'> BonnyFood Saklama Koşulları </a>
-            </li>
-            <li>
-              <a href='!#'> site Haritası </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className='footermenu__menubox'>
-        <div className='menubox-title'>
-          <p> Faydalı Bilgiler</p>
-        </div>
-        <div className='menubox-menu'>
-          <ul>
-            <li>
-              <a href='!#'> Çiçek Bakımı </a>
-            </li>
-            <li>
-              <a href='!#'> Çiçek Eşliğinde Notlar </a>
-            </li>
-            <li>
-              <a href='!#'> Çiçek Anlamları </a>
-            </li>
-            <li>
-              <a href='!#'> Özel Günler </a>
-            </li>
-            <li>
-              <a href='!#'> Mevsimlere Göre Çiçekler </a>
-            </li>
-            <li>
-              <a href='!#'> BonnyFood Saklama Koşulları </a>
-            </li>
-            <li>
-              <a href='!#'> site Haritası </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      ))
+      }
     </div>
   )
 }
 
 export default FooterMenu
+
---
+++ src/views/homepage/Footer/components/FooterMenu/footerMenuData.ts
@@ -0,0 +1,51 @@
+const footerMenuData = [
+    {
+        id: 1,
+        title: "Faydalı Bilgiler",
+        menu: [
+            {id:1, title:"Çiçek Bakımı"},
+            {id:2, title:"Çiçek Eşliğinde Notlar"},
+            {id:3, title:"Çiçek Anlamları"},
+            {id:4, title:"Özel Günler"},
+            {id:5, title:"Mevsimlere Göre Çiçekler"},
+            {id:6, title:"BonnyFood Saklama Koşulları"},
+            {id:7, title:"Site Haritası"}
+        ]
+    },
+    {
+        id: 2,
+        title: "Kurumsal",
+        menu: [
+            {id:1, title:"Hakkımızda"},
+            {id:2, title:"Kariyer"},
+            {id:3, title:"ÇiçekSepeti’nde Satış Yap"},
+            {id:4, title:"Kurumsal Müşterilerimiz"},
+            {id:5, title:"Reklamlarımız"},
+            {id:6, title:"Basında Biz"},
+            {id:7, title:"Kampanyalar"},
+            {id:8, title:"Vizyonumuz"}
+        ]
+    },
+    {
+        id: 3,
+        title: "İletişim",
+        menu: [
+            {id:1, title:"Bize Ulaşın"},
+            {id:2, title:"Sıkça Sorulan Sorular"},
+        ]
+    },
+    {
+        id: 4,
+        title: "Gizlilik Sözleşmesi",
+        menu: [
+            {id:1, title:"Mesafeli Satış Sözleşmesi"},
+            {id:2, title:"Bilgi Toplumu Hizmetleri"},
+            {id:3, title:"ÇiçekSepeti’nde Satış Yap"},
+            {id:4, title:"Gizlilik Sözleşmesi"},
+            {id:5, title:"Ödeme Seçenekleri"},
+            {id:6, title:"Hesap Bilgileri"},
+        ]
+    },
+]
+
+export default footerMenuData
